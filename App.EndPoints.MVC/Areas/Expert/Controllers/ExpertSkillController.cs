3364ec116a2b0a1fcb8200184e8e610448f11fb8
sina <sina1994zarabi@gmail.com>
2025-03-23 09:59:37 +0330

    Skill Expert

---
+++ App.EndPoints.MVC/Areas/Expert/Controllers/ExpertSkillController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.EndPoints.MVC.Areas.Expert.Controllers
+{
+    public class ExpertSkillController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
