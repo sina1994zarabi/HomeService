5e4ea0a0c7f982afe63bf80a2b36af6abacb9064
sina <sina1994zarabi@gmail.com>
2025-04-09 16:27:47 +0330

    Revised

--- App.Domain.Core/DTOs/AccountDto/AccountRegisterDto.cs
+++ App.Domain.Core/DTOs/AccountDto/AccountRegisterDto.cs
@@ -11,19 +11,19 @@ namespace App.Domain.Core.DTOs.AccountDto
 {
     public class AccountRegisterDto
     {
-        [Required]
+        [Required(ErrorMessage = "لطفا نام و نام خانوادگی خود را وارد کنید")]
         public string FullName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "لطفا ایمیل خود را وارد کنید")]
         [EmailAddress]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "لطفا نام کاربری خود را وارد کنید ")]
         public string Username { get; set; }
-        [Required]
+        [Required(ErrorMessage = "لطفا شماره تلفن خود را وارد کنید ")]
         public string PhoneNumber { get; set; }
-        [Required]
+        [Required(ErrorMessage = "لطفا رمز عبور را وارد کنید")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-        [Required]
+        [Required(ErrorMessage = "لطفا رمز عبور را تکرار نمایید")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "رمز عبور یکی نیست")]
         public string ConfirmedPassword { get; set; }
